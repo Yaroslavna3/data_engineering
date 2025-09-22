a153ea40b77028417e1c977b3cb7290adffd8e3e
Yaroslavna3 <yaratopalova@gmail.com>
2025-09-22 14:35:31 +0300

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,7 @@ FILE_ID = "1RRt9sN3xSrAoGq-4kGoO-AcTCTrZWqdq"
 file_url = f"https://drive.google.com/drive/folders/1RRt9sN3xSrAoGq-4kGoO-AcTCTrZWqdq"
 
 def main():
-    raw_data = pd.read_csv(file_url)
+    raw_data = pd.read_csv(file_url, sep=',')
     print("Первые 10 строк датасета:")
     print(raw_data.head(10))
 
