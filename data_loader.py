10e10b3b7287b67b78f0339f412e8be4176cca59
Yaroslavna3 <yaratopalova@gmail.com>
2025-09-22 14:42:00 +0300

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
 FILE_ID = "1RRt9sN3xSrAoGq-4kGoO-AcTCTrZWqdq"  
-file_url = f"https://drive.google.com/drive/folders/1RRt9sN3xSrAoGq-4kGoO-AcTCTrZWqdq"
+file_url = f"https://drive.google.com/uc?id=1RRt9sN3xSrAoGq-4kGoO-AcTCTrZWqdq&export=download"
 
 def main():
-    raw_data = pd.read_csv(file_url, sep=',')
-    print("Первые 10 строк датасета:")
-    print(raw_data.head(10))
+    try:
+        response = requests.get(file_url)
+        response.raise_for_status()  
+                       
+        raw_data = pd.read_csv(StringIO(response.text), sep=',')
+        print("Первые 10 строк датасета:")
+        print(raw_data.head(10))
+    except Exception as e:
+        print("Ошибка при загрузке:", e)
 
 if __name__ == "__main__":
     main()
