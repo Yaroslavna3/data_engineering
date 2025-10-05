2292049ac78bd0940db80e1f5a47ed9dca945ab1
Yaroslavna3 <yaratopalova@gmail.com>
2025-10-05 22:22:52 +0300

    Update api_reader_utf8.py

--- api_example/api_reader_utf8.py
+++ api_example/api_reader_utf8.py
@@ -1,1 +1,18 @@
+import requests
+import pandas as pd
 
+def get_cat_facts(limit=5):
+    
+    url = f"https://catfact.ninja/facts?limit={limit}"
+    response = requests.get(url)
+    response.raise_for_status()  
+
+    data = response.json()["data"]
+    df = pd.DataFrame(data)
+    return df
+
+if __name__ == "__main__":
+    df = get_cat_facts(10)
+    print(df.head())  
+    df.to_csv("cat_facts.csv", index=False)
+    print("\nДанные сохранены в cat_facts.csv")
